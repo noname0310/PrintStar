0a575cdd903d93fecc8137b72c68fc96ef041c90
noname <48761044+noname0310@users.noreply.github.com>
2021-01-06 10:12:14 +0900

    bug fix

--- PrintStar/PrintStar.c
+++ PrintStar/PrintStar.c
@@ -1,12 +1,11 @@
 #include "Graphics.h"
 
-#define PI 3.14159265358979323846
-#define point_max 50
-
 int main()
 {
 	const int width = 160 * 5;
 	const int height = 90 * 5;
+	const int point_max = 50;
+	const float pi = 3.14159265358979323846f;
 
 	Console.default_init();
 	Console.font_normalize(1);
@@ -18,7 +17,7 @@ int main()
 	int screen_center_x = width / 2;
 	int screen_center_y = height / 2;
 
-	Point* points = (Point*)malloc(sizeof(Point) * (point_max + 2));
+	Point* points = (Point*)malloc(sizeof(Point) * ((size_t)point_max + 2));
 	assert(points != NULL);
 	for (int star_point = 3; star_point < point_max; star_point+=2)
 	{
@@ -28,11 +27,11 @@ int main()
 			if (!((i - 90) % (360 / star_point)))
 			{
 				Point point = Point_new(
-					screen_center_x + (int)(cos(i * PI / 180) * radius),
-					screen_center_y + (int)(sin(i * PI / 180) * radius)
+					screen_center_x + (int)(cos(i * pi / 180) * radius),
+					screen_center_y + (int)(sin(i * pi / 180) * radius)
 				);
 				OOPTool.set_const_value(&points[point_counter++], &point, sizeof(Point));
-				graphics.f->draw_single(&graphics, L'*', point, 1);
+				graphics.f->draw_single(&graphics, L'&', point, 1);
 			}
 		}
 		for (int i = 0; i < star_point; i++)
